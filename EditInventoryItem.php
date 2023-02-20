12a52e212fe42c779cd4508037fa373bc488aed9
James Sing <87142332+ambivjaps@users.noreply.github.com>
2023-02-21 04:25:27 +0800

    Refined front end view

--- EditInventoryItem.php
+++ EditInventoryItem.php
@@ -29,50 +29,49 @@
         <section class="clean-block clean-catalog dark" style="background-color:#efe9ef;">
             <div class="container">
                 <div class="block-heading">
-                    <h2 style="margin-bottom: 17.2px;font-size: 54px;text-align: left;margin-top:64px; color:black;">Edit Inventory Item</h2>
+                    <h2 style="font-size: 54px;text-align: left;color:black;">Edit Inventory Item</h2>
                 </div>
                 <div class="content"></div>
             </div>
-            <div class="container profile profile-view" id="profile" style="background: #ffffff;width: 1354px;">
+            <div class="container profile profile-view" id="profile" style="background: #ffffff">
                 <div class="row">
                     <div class="col-md-12 alert-col relative">
                         <div class="alert alert-info alert-dismissible absolue center" role="alert"><button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button><span>Profile save with success</span></div>
                     </div>
                 </div>
                 <form>
-                    <div class="row profile-row" method="post">
+                    <div class="row profile-row" action="#" method="POST">
                         <div class="col-md-4 relative">
                             <div class="avatar">
                                 <div class="avatar-bg center" style="background: url(<?php echo $inv['ItemImg']; ?>); background-size: cover; background-position: 50% 50%;"></div>
                             </div>
-                            <input class="form-control form-control" type="file" name="avatar-file">
+                            <input class="form-control form-control my-3" type="file" id="ItemImg" name="avatar-file">
                         </div>
                         <div class="col-md-8">
-                            <hr>
                             <div class="row">
                                 <div class="col-md-2">
                                     <label class="col-form-label" for="item-id">Item ID </label>
-                                    <input class="form-control item" type="text" id="name-4" value="<?php echo $inv['ItemID']; ?>" required>
+                                    <input class="form-control item" type="text" id="ItemID" value="<?php echo $inv['ItemID']; ?>" required>
                                 </div>
                                 <div class="col-md-10">
                                     <label class="col-form-label" for="item-name">Item Name </label>
-                                    <input class="form-control item" type="text" id="name-3" value="<?php echo $inv['ItemName']; ?>" required>
+                                    <input class="form-control item" type="text" id="ItemName" value="<?php echo $inv['ItemName']; ?>" required>
                                 </div>
                                 <div class="col-md-12">
                                     <label class="col-form-label" for="item-desc">Item Description </label>
-                                    <textarea type="text" rows="10" class="form-control" id="#" required><?php echo $inv['ItemDesc']; ?></textarea>
+                                    <textarea type="text" rows="10" class="form-control" id="ItemDesc" required><?php echo $inv['ItemDesc']; ?></textarea>
                                 </div>
                                 <div class="col-md-4">
                                     <label class="col-form-label" for="item-qty">Item Quantity </label>
-                                    <input class="form-control item" type="text" id="name-6" value="<?php echo $inv['ItemQty']; ?>" required>
+                                    <input class="form-control item" type="text" id="ItemQty" value="<?php echo $inv['ItemQty']; ?>" required>
                                 </div>
                                 <div class="col-md-4">
                                     <label class="col-form-label" for="item-type">Item Type </label>
-                                    <input class="form-control item" type="text" id="name-1" value="<?php echo $inv['ItemType']; ?>" required>
+                                    <input class="form-control item" type="text" id="ItemType" value="<?php echo $inv['ItemType']; ?>" required>
                                 </div>
                                 <div class="col-md-4">
                                     <label class="col-form-label" for="item-price">Item Price (in Php) </label>
-                                    <input class="form-control item" type="text" id="name-1" value="<?php echo $inv['ItemPrice']; ?>" required>
+                                    <input class="form-control item" type="text" id="ItemPrice" value="<?php echo $inv['ItemPrice']; ?>" required>
                                 </div>
                             </div>
                             <hr>
