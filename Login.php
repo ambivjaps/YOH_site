cb26bc3a85f9b93f464cffcfa978a86811cad86e
James Sing <87142332+ambivjaps@users.noreply.github.com>
2023-03-02 04:58:45 +0800

    Removed whitespace

--- Login.php
+++ Login.php
@@ -82,7 +82,7 @@ if(!empty($_SESSION['cust_id'])) {
             <div class="container" style="--bs-primary: #fd0d72;--bs-primary-rgb: 253,13,114;--bs-body-bg: #ffffff;">
                 <div class="block-heading"><img style="padding-top: 0px;margin-left: 0px;margin-top: -9px;width: 231px;height: 201px;" src="assets/img/LOGOEXAMPLE.png"></div>
                 <h2 class="text-info" style="text-align: center;margin-top: -16px;margin-bottom: 25px;font-size: 41px;color: var(--bs-indigo);">Login</h2>
-                <form data-bss-hover-animate="pulse" style="width: 554px;max-width: 753px;margin-bottom: 41px;min-height: 399px;margin-left: 374px;margin-right: 404px;margin-top: 20px;min-width: 205px;color: var(--bs-purple);background: #ffffff;--bs-body-bg: var(--bs-indigo);box-shadow: 0px 0px var(--bs-indigo);--bs-info: #e03b80;--bs-info-rgb: 224,59,128;height: 404px;"  method="post" >
+                <form data-bss-hover-animate="pulse" style="width: 554px;max-width: 753px;margin-bottom: 41px;min-height: 399px;margin-left: 374px;margin-right: 404px;margin-top: 20px;min-width: 205px;color: var(--bs-purple);background: #ffffff;--bs-body-bg: var(--bs-indigo);box-shadow: 0px 0px var(--bs-indigo);--bs-info: #e03b80;--bs-info-rgb: 224,59,128;height: 404px;"  method="post">
                 <?php if (isset($_GET['registrationSuccess']) && $_GET['registrationSuccess'] === 'true') { ?>
                     <div class="alert alert-success" role="alert">
                         Successfully registered account. Please login.
@@ -93,7 +93,7 @@ if(!empty($_SESSION['cust_id'])) {
 
                     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 
-                     <div class="g-recaptcha" data-sitekey="6LdarEwkAAAAANs4-E8qkamwsZJYF_-OBWprGpSi"></div> 
+                    <div class="g-recaptcha" data-sitekey="6LdarEwkAAAAANs4-E8qkamwsZJYF_-OBWprGpSi"></div> 
 
                     <div class="mb-3"></div><a href="ForgotPass.php" style="margin-left: 145px;margin-top: -8px;margin-bottom: 173px;margin-right: 12px;color: rgb(111,66,193);">Forgot your Password?</a><button class="btn btn-primary" type="submit" style="margin-left: 162px;min-width: 133px;max-width: 180px;margin-bottom: 10px;margin-right: 195px;padding-left: 0px;padding-right: 0px;padding-top: 4px;padding-bottom: 4px;height: 32px;margin-top: 82px;width: 147px;">
                     Login</button><a href="registration.php" style="margin-left: 86px;margin-top: -8px;margin-bottom: 173px;margin-right: 12px;color: rgb(111,66,193);">Don't have an account? Register Here</a>
