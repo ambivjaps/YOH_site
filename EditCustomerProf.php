2ce4b9e8837116c933b2c8490ae2b4e48212ce36
James Sing <87142332+ambivjaps@users.noreply.github.com>
2023-03-05 05:21:53 +0800

    Added edit function for customer profile

--- EditCustomerProf.php
+++ EditCustomerProf.php
@@ -10,14 +10,61 @@
 
     require 'layouts/Header.php';
 
-    if(isset($_SESSION['cust_id'])) {
-		$id = mysqli_real_escape_string($con, $_SESSION['cust_id']);
-		$item = "SELECT * FROM cust_profile WHERE c_id = $id";
+    if(isset($_GET['id'])) {
+		$id = mysqli_real_escape_string($con, $_GET['id']);
+		$item = "SELECT * FROM cust_profile WHERE id = $id";
 		$result = mysqli_query($con, $item);
-		$c_prof = mysqli_fetch_assoc($result);
+		$profile = mysqli_fetch_assoc($result);
 
 		mysqli_free_result($result);
 	}
+
+?>
+
+<?php 
+    if(isset($_POST['submit'])) {
+        $PID = $profile['id'];
+    
+        $c_name = mysqli_real_escape_string($con, $_POST['c_name']);
+        $c_avatar = mysqli_real_escape_string($con, $_POST['c_avatar']);
+        $email = mysqli_real_escape_string($con, $_POST['email']);
+        $address = mysqli_real_escape_string($con, $_POST['address']);
+        
+        $region = mysqli_real_escape_string($con, $_POST['region']);
+        $city = mysqli_real_escape_string($con, $_POST['city']);
+        $street = mysqli_real_escape_string($con, $_POST['street']);
+        $barangay = mysqli_real_escape_string($con, $_POST['barangay']);
+        
+        $phone_no = mysqli_real_escape_string($con, $_POST['phone_no']);
+        $zip_code = mysqli_real_escape_string($con, $_POST['zip_code']);
+        $unit_no = mysqli_real_escape_string($con, $_POST['unit_no']);
+    
+        $query = "UPDATE cust_profile SET c_name='$c_name',c_avatar='$c_avatar',email='$email',address='$address',region='$region',city='$city',street='$street',barangay='$barangay',phone_no='$phone_no',zip_code='$zip_code',unit_no='$unit_no' WHERE id=$PID";
+        $query_run = mysqli_query($con, $query);
+    
+        if($query_run) {
+            $_SESSION['c_name'] = $_POST['c_name'];
+            $_SESSION['c_avatar'] = $_POST['c_avatar'];
+            $_SESSION['email'] = $_POST['email'];
+            $_SESSION['address'] = $_POST['address'];
+
+            $_SESSION['region'] = $_POST['region'];
+            $_SESSION['city'] = $_POST['city'];
+            $_SESSION['street'] = $_POST['street'];
+            $_SESSION['barangay'] = $_POST['barangay'];
+
+            $_SESSION['phone_no'] = $_POST['phone_no'];
+            $_SESSION['zip_code'] = $_POST['zip_code'];
+            $_SESSION['unit_no'] = $_POST['unit_no'];
+
+            mysqli_close($con);
+            header("Location: ProfileAccntView.php");
+            exit();
+
+        } else {
+            echo "<script> alert('Problem occured.') </script>";
+        }
+    }
 ?>
 
 <title> Edit Customer Profile | Yarn Over Hook </title>
@@ -26,75 +73,64 @@
 
 <?php require 'layouts/nav.php';?>
 
-    <main class="page catalog-page">
-        <section class="clean-block clean-catalog dark" style="min-height: 17px;height: 971px; background-color:#efe9ef;">
-            <div class="container">
-                <div class="block-heading">
-                <h2 style="margin-bottom: 17.2px;font-size: 54px;text-align: left;margin-top:64px; color:black; font-weight:bold;">Edit Customer Profile</h2>
-                </div>
-                <div class="content"></div>
-            </div>
-            <div class="container profile profile-view" id="profile" style="background: #ffffff;width: 1354px;">
-                <form action="EditCustomerProf.php" method="POST">
-                    <div class="row profile-row">
-                        <div class="col-md-4 relative">
-                            <div class="avatar">
-                                <div class="avatar-bg center"></div>
-                            </div><input class="form-control form-control" type="file" name="avatar-file">
-                        </div>
-                        <div class="col-md-8">
-                            <h1></h1>
-                            <hr>
-                            <div class="row">
-                                <div class="col-sm-12 col-md-6">
-                                    <div class="form-group mb-3">
-                                        <label class="form-label" style="margin-bottom: 8px;margin-top: -7px;">Full Name</label>
-                                        <input class="form-control" type="text" name="c_name" <?php if(isset($_SESSION['cust_id'])){ echo 'value="'.$c_prof['c_name'].'"'; } ?>></div>
-                                </div>
-                                <div class="col-sm-12 col-md-6">
-                                    <label class="form-label" for="name" style="margin-left: 24px;padding-left: -6px;margin-bottom: 15px;margin-top: 8px;">Street
-                                    <input class="form-control item" type="text" name="street" id="name-1" style="width: 121px;margin-bottom: 4px;min-width: 76px;" required="" <?php if(isset($_SESSION['cust_id'])){ echo 'value="'.$c_prof['street'].'"'; } ?>></label>
-                                    <label class="form-label" for="name" style="margin-left: 31px;">Building No.
-                                    <input class="form-control item" type="text" name="building" id="name-9" style="width: 121px;margin-bottom: 4px;min-width: 76px;" required="" <?php if(isset($_SESSION['cust_id'])){ echo 'value="'.$c_prof['building'].'"'; } ?>></label></div>
-                                </div>
-                            <div class="row" style="margin-left: 28px;">
-                                <div class="col-sm-12 col-md-6 col-xxl-6" style="margin-left: -41px;">
-                                    <div class="form-group mb-3" style="width: 411.656px;">
-                                    <label class="form-label">Email Address</label>
-                                    <input class="form-control" type="email"  name="email" required="" <?php if(isset($_SESSION['cust_id'])){ echo 'value="'.$c_prof['email'].'"'; } ?>></div>
-                                </div>
-                                <div class="col">
-                                    <label class="form-label" for="name" style="margin-left: 47px;">Zip Code<br>
-                                    <input class="form-control item" type="text" name="zip_code" id="name-7" style="width: 121px;margin-bottom: 4px;min-width: 76px;" required="" <?php if(isset($_SESSION['cust_id'])){ echo 'value="'.$c_prof['zip_code'].'"'; } ?>></label>
-                                    <label class="form-label" for="name" style="margin-left: 28px;">Unit No.
-                                    <input class="form-control item" type="text" name="unit_no" id="name-10" style="width: 121px;margin-bottom: 4px;min-width: 76px;" required="" <?php if(isset($_SESSION['cust_id'])){ echo 'value="'.$c_prof['unit_no'].'"'; } ?>></label>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-sm-12 col-md-6">
-                                    <label class="form-label" for="name" style="margin-left: 31px;">Region
-                                    <input class="form-control item" type="text" name="region" id="name-4" style="width: 121px;margin-bottom: 4px;" required="" <?php if(isset($_SESSION['cust_id'])){ echo 'value="'.$c_prof['region'].'"'; } ?>></label>
-                                
-                                    <label class="form-label" for="name" style="margin-left: 91px;width: 117px;">City
-                                    <input class="form-control item" type="text" name="city" id="name-8" style="width: 121px;margin-bottom: 4px;" required="" <?php if(isset($_SESSION['cust_id'])){ echo 'value="'.$c_prof['city'].'"'; } ?>></label></div>
-                                
-                                <div class="col-sm-12 col-md-6">
-                                    <div class="form-group mb-3"><label class="form-label">Phone Number</label>
-                                    <input class="form-control" type="text" name="phone_no" required="" <?php if(isset($_SESSION['cust_id'])){ echo 'value="'.$c_prof['phone_no'].'"'; } ?>></div>
-                                </div>
-                            </div>
-                            <hr>
-                            <div class="row">
-                                <div class="col-md-12 content-right">
-                                    <button class="btn btn-primary form-btn" type="submit" id="submit" name="submit">SAVE </button>
-                                    <button class="btn btn-danger form-btn" type="reset" id="reset" name="reset">CANCEL </button>
-                                </div>
-                            </div>
-                        </div>
+    <div class="container my-5">
+
+        <h1> Edit Profile </h1>
+        <div class="form-group">
+            <form action="EditCustomerProf.php?id=<?php echo $profile['id'] ?>" method="POST">
+                <div class="row my-3">
+                    <div class="col-md-6">
+                        <label>Avatar</label>
+                        <input type="file" class="form-control form-control my-3" name="c_avatar">
                     </div>
-                </form>
-            </div>
-        </section>
-    </main>
+                    <div class="col-md-12">
+                        <label>Name</label>
+                        <input type="text" name="c_name" id="c_name" class="form-control" value="<?php echo $profile['c_name'] ?>">
+                    </div>
+                    <div class="col-md-12">
+                        <label>Address</label>
+                        <input type="text" name="address" id="address" class="form-control" value="<?php echo $profile['address'] ?>">
+                    </div>
+       
+                    <div class="col-md-4">
+                        <label>Street</label>
+                        <input type="text" name="street" id="street" class="form-control" value="<?php echo $profile['street'] ?>">
+                    </div>
+                    <div class="col-md-4">
+                        <label>City</label>
+                        <input type="text" name="city" id="city" class="form-control" value="<?php echo $profile['city'] ?>">
+                    </div>
+                    <div class="col-md-4">
+                        <label>Barangay</label>
+                        <input type="text" name="barangay" id="barangay" class="form-control" value="<?php echo $profile['barangay'] ?>">
+                    </div>
+                    <div class="col-md-4">
+                        <label>Unit Number</label>
+                        <input type="text" name="unit_no" id="unit_no" class="form-control" value="<?php echo $profile['unit_no'] ?>">
+                    </div>
+                    <div class="col-md-4">
+                        <label>Zip Code</label>
+                        <input type="text" name="zip_code" id="zip_code" class="form-control" value="<?php echo $profile['zip_code'] ?>">
+                    </div>
+                    <div class="col-md-4">
+                        <label>Region</label>
+                        <input type="text" name="region" id="region" class="form-control" value="<?php echo $profile['region'] ?>">
+                    </div>
+                    <div class="col-md-6">
+                        <label>E-mail Address</label>
+                        <input type="email" name="email" id="email" class="form-control" value="<?php echo $profile['email'] ?>">
+                    </div>
+                    <div class="col-md-6">
+                        <label>Phone Number</label>
+                        <input type="text" name="phone_no" id="phone_no" class="form-control" value="<?php echo $profile['phone_no'] ?>">
+                    </div>
+                    <div class="button-group float-end">
+                        <input class="btn btn-success mt-3" type="submit" id="submit" name="submit" value="Submit">
+                        <input class="btn btn-danger mt-3" type="reset" id="reset" value="Reset Form">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
     
 <?php require 'layouts/Footer.php';?>
--- ProfileAcct.php
+++ ProfileAcct.php
@@ -26,8 +26,105 @@
 
 <?php require 'layouts/nav.php';?>
 
-    <div class="container my-5">
-        <h1> Page under construction. </h1>
+    <?php if($profile): ?>
+
+    <main class="page blog-post">
+    <section class="clean-block clean-post dark" style="background-color:#efe9ef; border:none; ">
+        <div class="container">
+
+        <form class="mb-3" action="ProfileAcct.php" method="POST">
+			<a class="btn btn-dark" href="EditCustomerProf.php?id=<?php echo $profile['id'] ?>" type="submit" name="edit" role="button">Edit Record</a>
+			<input type="hidden" class="delete_id" name="delete_id" value="<?php echo $profile['id']; ?>">
+			<a class="btn btn-dark" href="javascript:void(0)" type="submit" name="delete" role="button">Delete Record</a>
+		</form> 
+
+    <div class="row gutters">
+    <div class="col-xl-3 col-lg-3 col-md-12 col-sm-12 col-12">
+    <div class="card h-100">
+        <div class="card-body" style="background: #f4e3ff;">
+            <div class="account-settings">
+                <div class="user-profile">
+                    <div class="user-avatar" >
+                        <img src="<?php echo $profile['c_avatar']; ?>" style="height:200px; width:200px;">
+                    </div>
+                    <h5 class="user-name" style="font-weight: bold; font-size:40px; color: var(--bs-indigo);"><?php echo $profile['c_name']; ?></h5>
+                    <p style="font-weight: bold; "><?php echo $profile['email']; ?></p>
+                    <p style="font-weight: bold; "><?php echo $profile['phone_no']; ?></p>
+                </div>
+            </div>
+
+        </div>
     </div>
+    </div>
+    <div class="col-xl-9 col-lg-9 col-md-12 col-sm-12 col-12">
+    <div class="card h-100">
+        <div class="card-body" style="background: #f4e3ff;">
+            <div class="row gutters">
+                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                    <h6 style="font-weight: bold; font-size:40px; text-align:center; " >Shipping Details</h6>
+                    <hr>
+                </div>
+                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                    <div class="form-group">
+                        <label style="font-weight:bold;font-size:30px; ">Customer ID</label>
+                        <p class="rounded" style="font-size:15px;background:#cbc3e3; font-weight:bold; text-align:center;"><?php echo $profile['c_id']; ?></p>
+                    </div>
+                </div>
+                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                    <div class="form-group">
+                        <label style="font-weight:bold;font-size:30px; ">Address</label>
+                        <p class="rounded" style="font-size:15px;background:#cbc3e3; font-weight:bold; text-align:center;"><?php echo $profile['address']; ?></p>
+                    </div>
+                </div>
+                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                    <div class="form-group">
+                        <label style="font-weight:bold; font-size:30px; ">Unit Number</label>
+                        <p class="rounded" style="font-size:15px;background:#cbc3e3; font-weight:bold; text-align:center;"><?php echo $profile['unit_no']; ?></p>
+                    </div>
+                </div>
+                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                    <div class="form-group">
+                        <label style="font-weight:bold;font-size:30px; " >Barangay</label>
+                        <p class="rounded" style="font-size:15px;background:#cbc3e3; font-weight:bold; text-align:center;"><?php echo $profile['barangay']; ?></p>
+                    </div>
+                </div>
+                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                    <div class="form-group">
+                        <label style="font-weight:bold;font-size:30px; ">City</label>
+                        <p class="rounded" style="font-size:15px;background:#cbc3e3; font-weight:bold; text-align:center;"><?php echo $profile['city']; ?></p>
+                    </div>
+                </div>
+                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                    <div class="form-group">
+                        <label style="font-weight:bold;font-size:30px; ">Street</label>
+                        <p class="rounded" style="font-size:15px;background:#cbc3e3; font-weight:bold; text-align:center;"><?php echo $profile['street']; ?></p>
+                    </div>
+                </div>
+                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                    <div class="form-group">
+                        <label style="font-weight:bold;font-size:30px; ">Region</label>
+                        <p class="rounded" style="font-size:15px;background:#cbc3e3; font-weight:bold; text-align:center;"><?php echo $profile['region']; ?></p>
+                    </div>
+                </div>
+                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-10" style="position:center;">
+                    <div class="form-group">
+                        <label style="font-weight:bold; font-size:30px;">Zip Code</label>
+                        <p class="rounded" style="font-size:15px;background:#cbc3e3; font-weight:bold; text-align:center;"><?php echo $profile['zip_code']; ?></p>
+                    </div>
+                </div>
+            </div>     
+        </div>
+    </div>
+    </div>
+    </div>
+    </div>
+            <?php else: ?>
+                <div class="container my-5">
+                    <h2> Oops.. Page not found. Please try again. </h2>
+                </div>
+            <?php endif ?>
+
+        </section>
+    </main>
         
 <?php require 'layouts/Footer.php';?>
