27cc888e26421a343635fc429c1b5bb470db92d6
James Sing <87142332+ambivjaps@users.noreply.github.com>
2023-03-05 19:48:42 +0800

    Edit Inventory now working

--- EditInventoryItem.php
+++ EditInventoryItem.php
@@ -18,85 +18,66 @@
 	}
 ?>
 
-<title> Edit Inventory Item: <?php echo $inv['ItemName']; ?> | Yarn Over Hook </title>
+<?php 
+     if(isset($_POST['submit'])) {
+        $InvID = $inv['ItemID'];
+    
+        $ItemName = mysqli_real_escape_string($con, $_POST['ItemName']);
+        $ItemDesc = mysqli_real_escape_string($con, $_POST['ItemDesc']);
+    
+        $query = "UPDATE inventory_db SET ItemName='$ItemName',ItemDesc='$ItemDesc' WHERE ItemID=$InvID";
+        $query_run = mysqli_query($con, $query);
+    
+        if($query_run) {
+            $_SESSION['ItemName'] = $_POST['ItemName'];
+            $_SESSION['ItemDesc'] = $_POST['ItemDesc'];
+
+            mysqli_close($con);
+            header("Location: Inventory.php");
+            exit();
+
+        } else {
+            echo "<script> alert('Problem occured.') </script>";
+        }
+    }
+?>
+
+<title> Edit Inventory Item | Yarn Over Hook </title>
 
 <body class="d-flex flex-column min-vh-100">
 
 <?php require 'layouts/nav.php';?>
 
-    <?php if($inv): ?>
-        
-    <main class="page catalog-page">
-        <section class="clean-block clean-catalog dark" style="background-color:#efe9ef;">
-            <div class="container">
-                <div class="block-heading">
-                <h2 style="margin-bottom: 17.2px;font-size: 54px;text-align: left;margin-top:64px; color:black; font-weight:bold;">Edit Inventory Item</h2>
-                </div>
-                <div class="content"></div>
-            </div>
-            <div class="container profile profile-view" id="profile" style="background: #ffffff">
-                <div class="row">
-                    <div class="col-md-12 alert-col relative">
-                        <div class="alert alert-info alert-dismissible absolue center" role="alert"><button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button><span>Profile save with success</span></div>
-                    </div>
-                </div>
-                <form action="EditInventoryItem.php?id=<?php echo $inv['id']; ?>" method="POST">
-                    <div class="row profile-row">
-                        <div class="col-md-4 relative">
-                            <div class="avatar">
-                                <div class="avatar-bg center" style="background: url(<?php echo $inv['ItemImg']; ?>); background-size: cover; background-position: 50% 50%;"></div>
-                            </div>
-                            <input class="form-control form-control my-3" type="file" name="avatar-file">
-                        </div>
-                        <div class="col-md-8">
-                            <div class="row">
-                                <div class="col-md-2">
-                                    <label class="col-form-label" for="item-id">Item ID </label>
-                                    <input class="form-control item" type="text" name="ItemID" value="<?php echo $inv['ItemID']; ?>" required>
-                                </div>
-                                <div class="col-md-10">
-                                    <label class="col-form-label" for="item-name">Item Name </label>
-                                    <input class="form-control item" type="text" name="ItemName" value="<?php echo $inv['ItemName']; ?>" required>
-                                </div>
-                                <div class="col-md-12">
-                                    <label class="col-form-label" for="item-desc">Item Description </label>
-                                    <textarea type="text" rows="10" class="form-control" name="ItemDesc" required><?php echo $inv['ItemDesc']; ?></textarea>
-                                </div>
-                                <div class="col-md-4">
-                                    <label class="col-form-label" for="item-qty">Item Quantity </label>
-                                    <input class="form-control item" type="text" name="ItemQty" value="<?php echo $inv['ItemQty']; ?>" required>
-                                </div>
-                                <div class="col-md-4">
-                                    <label class="col-form-label" for="item-type">Item Type </label>
-                                    <select class="form-control item" value="<?php echo $inv['ItemType']; ?>"  required>
-                                    <option value="raw">Raw</option>
-                                    <option value="comp">Finished</option>
-                                    </optgroup>
-                                    </select> 
-                                </div>
-                                <div class="col-md-4">
-                                    <label class="col-form-label" for="item-price">Item Price (in Php) </label>
-                                    <input class="form-control item" type="text" name="ItemPrice" value="<?php echo $inv['ItemPrice']; ?>" required>
-                                </div>
-                            </div>
-                            <hr>
-                            <div class="row">
-                                <div class="col-md-12 content-right">
-                                    <a href = "Inventory.php"><button class="btn btn-primary form-btn" type="submit" id="submit" name="submit" value="SAVE">SAVE</a>
-                                  <a href = "Inventory.php">  <button class="btn btn-danger form-btn" type="submit" id="reset" value="CANCEL">CANCEL</a>
-                            </div>
-                        </div>
-                    </div>
-                </form>
-            </div>
+<?php if($inv): ?>
 
-            <?php else: ?>
-                <div class="container my-5">
-                    <h2> Oops.. Page not found. Please try again. </h2>
+    <div class="container my-5">
+
+        <h1> Edit Profile </h1>
+        <div class="form-group">
+            <form action="EditInventoryItem.php?id=<?php echo $inv['ItemID'] ?>" method="POST">
+                <div class="row my-3">
+                    <div class="col-md-12">
+                        <label>Name</label>
+                        <input type="text" name="ItemName" id="ItemName" class="form-control" value="<?php echo $inv['ItemName'] ?>">
+                    </div>
+                    <div class="col-md-12">
+                        <label>Description</label>
+                        <input type="text" name="ItemDesc" id="ItemDesc" class="form-control" value="<?php echo $inv['ItemDesc'] ?>">
+                    </div>
+                    <div class="button-group float-end">
+                        <input class="btn btn-success mt-3" type="submit" id="submit" name="submit" value="Submit">
+                        <input class="btn btn-danger mt-3" type="reset" id="reset" value="Reset Form">
+                    </div>
                 </div>
-            <?php endif ?>
-        
-        </section>
-    </main>
-    
+            </form>
+        </div>
+    </div>
+
+    <?php else: ?>
+        <div class="container my-5">
+            <h2> Oops.. Page not found. Please try again. </h2>
+        </div>
+    <?php endif ?>
+
+
 <?php require 'layouts/Footer.php';?>
