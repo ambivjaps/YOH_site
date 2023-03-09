d41c996ddebd04e7fa567021c3c198742be99e1e
James Sing <87142332+ambivjaps@users.noreply.github.com>
2023-03-10 05:03:04 +0800

    Removed explore buttons

--- views/customer_home.php
+++ views/customer_home.php
@@ -152,21 +152,18 @@
                         <div class="h-100 p-5 text-white bg-dark rounded-1 yoh-card-ins" style="background-image: linear-gradient(to bottom, rgba(0,0,0,0.4) 0%,rgba(0,0,0,0.5) 100%), url('assets/img/home/IMG_7412.png');">
                             <h1 class="pt-5 my-3 lh-1" style="text-shadow: #000 1px 0 5px;"> Headwear </h1>
                             <p style="text-shadow: #000 1px 0 5px;">Express yourself with our headwear collection.</p>
-                            <a class="btn btn-light mt-2" href="#" role="button">Explore <i class="fas fa-caret-right"></i></a>
                         </div>
                     </div>
                     <div class="col-md-4 my-2">
                         <div class="h-100 p-5 text-white bg-dark rounded-1 yoh-card-ins" style="background-image: linear-gradient(to bottom, rgba(0,0,0,0.4) 0%,rgba(0,0,0,0.5) 100%), url('assets/img/home/IMG_7409.PNG');">
                             <h1 class="pt-5 my-3 lh-1" style="text-shadow: #000 1px 0 5px;"> Minis </h1>
                             <p style="text-shadow: #000 1px 0 5px;">Get yourself a cute personal companion to accompany you through the day.</p>
-                            <a class="btn btn-light mt-2" href="#" role="button">Explore <i class="fas fa-caret-right"></i></a>
                         </div>
                     </div>
                     <div class="col-md-4 my-2">
                         <div class="h-100 p-5 text-white bg-dark rounded-1 yoh-card-ins" style="background-image: linear-gradient(to bottom, rgba(0,0,0,0.4) 0%,rgba(0,0,0,0.5) 100%), url('assets/img/home/IMG_7413.PNG');">
                             <h1 class="pt-5 my-3 lh-1" style="text-shadow: #000 1px 0 5px;"> Accessories </h1>
                             <p style="text-shadow: #000 1px 0 5px;">Adorable accessories to attach to your personal belongings.</p>
-                            <a class="btn btn-light mt-2" href="#" role="button">Explore <i class="fas fa-caret-right"></i></a>
                         </div>
                     </div>
                 </div>
