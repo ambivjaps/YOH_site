e10a89887302b529d53681c2f60b08e784968a9a
James Sing <87142332+ambivjaps@users.noreply.github.com>
2023-03-12 05:37:04 +0800

    Updated userprofile

--- UserProfile.php
+++ UserProfile.php
@@ -20,15 +20,20 @@
 		mysqli_free_result($result);
 
         $cust_prof = mysqli_real_escape_string($con, $_SESSION['login_id']);
-		$main = "SELECT * FROM cust_profile WHERE c_id = $cust_prof AND current_profile='1'";
+		$main = "SELECT * FROM cust_profile WHERE c_id = $cust_prof AND cust_status='1'";
 		$result_prof = mysqli_query($con, $main);
 		$select = mysqli_fetch_all($result_prof, MYSQLI_ASSOC);
 
 		mysqli_free_result($result_prof);
 
-		$other = "SELECT * FROM cust_profile WHERE c_id = $cust_prof AND current_profile='0'";
+		$other = "SELECT * FROM cust_profile WHERE c_id = $cust_prof AND cust_status='0'";
+        $other_count = "SELECT COUNT(*) FROM cust_profile WHERE c_id = $cust_prof AND cust_status='0'";
+
 		$result_choice = mysqli_query($con, $other);
+        $result_count = mysqli_query($con, $other_count);
+
 		$option = mysqli_fetch_all($result_choice, MYSQLI_ASSOC);
+        $count = mysqli_fetch_all($result_count, MYSQLI_ASSOC);
 
 		mysqli_free_result($result_choice);
 	}
@@ -40,23 +45,17 @@
         $new_id = $_POST['selected_prof'];
         $old_id = $_POST['old_id'];
 
-        $query = "UPDATE cust_profile SET current_profile='1' WHERE id=$new_id";
-        $query2 = "UPDATE cust_profile SET current_profile='0' WHERE id=$old_id";
+        $query = "UPDATE cust_profile SET cust_status='1' WHERE id=$new_id";
+        $query2 = "UPDATE cust_profile SET cust_status='0' WHERE id=$old_id";
 
         $query_run = mysqli_query($con, $query);
         $query_run2 = mysqli_query($con, $query2);
     
-        if($query_run) {
-            if($query_run2) {
-
-            }
-            
+        if($query_run && $query_run2) {
             header("Location: UserProfile.php");
             mysqli_close($con);
+
             exit();
-
-            
-
         } else {
             echo "<script> alert('Problem occured.') </script>";
         }
@@ -74,10 +73,10 @@
         <?php if($user): ?>
 
         <div class="row my-3">
-            <h3><strong> User Profile </strong></h3><hr>
+            <h3><strong> <i class="fas fa-user"></i> User Profile </strong></h3><hr>
 
             <div class="col-md-2">
-                <img class="img-fluid rounded" src="assets/img/default/default_user.jpg">
+                <img class="img-fluid rounded" src="assets/img/default/default_user.jpg" title="<?php echo $user['cust_name'] ?>" alt="<?php echo $user['cust_name'] ?>">
             </div>
 
             <div class="col-md-10">
@@ -85,32 +84,40 @@
                 <p> Address: <?php echo $user['cust_address'] ?> </p>
                 <p> Phone Number: <?php echo $user['cust_phone'] ?> </p>
                 <p> Email: <?php echo $user['cust_email'] ?> </p>
-                <p> User since <?php echo date("F d, Y", strtotime($user['date'])); ?>  </p>
+                <p> User since <i><?php echo date("F d, Y", strtotime($user['date'])); ?></i>  </p>
+            </div>
+
+            <div class="float-end mt-2">
+                <a class="btn btn-dark" href="#" role="button"> Edit avatar </a>
+                <a class="btn btn-dark" href="#" role="button"> Change password </a>
             </div>
         </div>
 
         <hr><div class="row my-3">
-        <h3><strong> Customer Profile </strong></h3>
+        <h3><strong> <i class="fas fa-address-card"></i> Customer Records </strong></h3>
 
         <?php foreach($select as $prof): ?>
+            <div class="float-end mt-2 mb-5">
+                <a class="btn btn-dark" href="ProfileAccntView.php" role="button"> See your customer profiles </a>
+                <a class="btn btn-dark" href="OrderPageCust.php" role="button"> See your orders </a>
+            </div>
+            
             <p> Current customer profile used: <strong> <?php echo $prof['c_label']; ?> </strong> </p>
             <table class="table table-striped table-hover table-sm mt-2">
                 <tr>
                     <th> Name </th>
                     <th> Address </th>
                     <th> E-mail </th>
-                    <th> Phone Number </th>
                     <th> Action </th>
                 </tr>
 
                 <tr><td> <?php echo $prof['c_name']; ?> </td>
                 <td> <?php echo $prof['address']; ?>  </td>
-                <td> <?php echo $prof['email']; ?> </td>
                 <td> <?php echo $prof['phone_no']; ?> </td>
                 <td> <a class="btn btn-sm btn-primary" href="ProfileAccnt.php?id=<?php echo $prof['id'] ?>" role="button"><i class="fas fa-eye"></i> View</a> </td></tr>
             </table>
         <?php endforeach; ?>
-            
+
             <div class="form-group">
                 <form action="UserProfile.php?id=<?php echo $_SESSION['login_id'] ?>" method="POST">
                     <div class="row my-3">
@@ -131,15 +138,7 @@
                     </div>
                 </form>
             </div>
-        </div>
-
-        <hr><div class="row my-3">
-            <h3><strong> User Settings </strong></h3>
-            <div class="float-end mt-2">
-                <a class="btn btn-dark" href="ProfileAccntView.php" role="button"> See your customer profiles </a>
-                <a class="btn btn-dark" href="OrderPageCust.php" role="button"> See your orders </a>
-                <a class="btn btn-dark" href="ResetPass.php" role="button"> Change password </a>
-            </div>
+        
         </div>
 
         <?php else: ?>
