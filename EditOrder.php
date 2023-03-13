0f53447c56642eb52db95a0b346d9aa470d41bb7
James Sing <87142332+ambivjaps@users.noreply.github.com>
2023-03-13 08:31:50 +0800

    Refined EditOrder

--- EditOrder.php
+++ EditOrder.php
@@ -10,6 +10,16 @@
 
     require 'layouts/Header.php';
 
+    $inv = "SELECT * FROM inventory_db ORDER BY ItemID";
+	$result_inv = mysqli_query($con, $inv);
+	$inv_item = mysqli_fetch_all($result_inv, MYSQLI_ASSOC);
+	mysqli_free_result($result_inv);
+
+    $prof = "SELECT * FROM cust_profile WHERE cust_status = '1'";
+	$result_prof = mysqli_query($con, $prof);
+	$prof_sel = mysqli_fetch_all($result_prof, MYSQLI_ASSOC);
+	mysqli_free_result($result_prof);
+
     if(isset($_GET['id'])) {
 		$id = mysqli_real_escape_string($con, $_GET['id']);
 
@@ -27,14 +37,35 @@
 <?php 
     if(isset($_POST['edit_order'])) {
         $OID = $order['OrderID'];
+
+        $CustProf = $_POST['CustProf'];
+        $InvItem = $_POST['InvItem'];
+        $OrderType = $_POST['OrderType'];
+
+        $OrderQty = mysqli_real_escape_string($con, $_POST['OrderQty']);
+        $courier_id = mysqli_real_escape_string($con, $_POST['courier_id']);
+        $tracking_no = mysqli_real_escape_string($con, $_POST['tracking_no']);
+
+        if($OrderType == 'In Process'){
+            $query = "UPDATE orders_db SET TypeID='1' WHERE OrderID=$OID";
+            $query_run = mysqli_query($con, $query);
+        } else if($OrderType == 'Completed') {
+            $query = "UPDATE orders_db SET TypeID='2' WHERE OrderID=$OID";
+            $query_run = mysqli_query($con, $query);
+        } else {
+            die;
+        }
     
-        $OrderType = mysqli_real_escape_string($con, $_POST['OrderType']);
-    
-        $query = "UPDATE orders_db SET OrderType='$OrderType' WHERE OrderID=$OID";
+        $query = "UPDATE orders_db SET c_id='$CustProf',ItemID='$InvItem',OrderType='$OrderType',OrderQty='$OrderQty',courier_id='$courier_id',tracking_no='$tracking_no' WHERE OrderID=$OID";
         $query_run = mysqli_query($con, $query);
     
         if($query_run) {
+            $_SESSION['CustProf'] = $_POST['CustProf'];
+            $_SESSION['InvItem'] = $_POST['InvItem'];
             $_SESSION['OrderType'] = $_POST['OrderType'];
+            $_SESSION['OrderQty'] = $_POST['OrderQty'];
+            $_SESSION['courier_id'] = $_POST['courier_id'];
+            $_SESSION['tracking_no'] = $_POST['tracking_no'];
 
             mysqli_close($con);
             header("Location: OrdersAdminView.php");
@@ -60,11 +91,54 @@
 
         <h1> Edit Order </h1>
         <div class="form-group">
-            <form action="EditOrder.php?id=<?php echo $order['id'] ?>" method="POST" id="form">
+            <form action="EditOrder.php?id=<?php echo $order['OrderID'] ?>" method="POST" id="form">
                 <div class="row my-3">
+                    <h3 class="my-3"> Order Details </h3>
+
                     <div class="col-md-12">
-                        <label>Order Type</label>
-                        <input type="text" name="OrderType" id="OrderType" class="form-control" value="<?php echo $order['OrderType'] ?>">
+                        <label>Customer (Current: <?php echo $order['c_name'] ?>)</label>
+                        <select class="form-select" id="CustProf" name="CustProf" aria-label=".form-select example">
+                            <?php foreach($prof_sel as $prof): ?>
+                            <option value="<?php echo $prof['c_id'] ?>"><?php echo $prof['c_name'] ?></option>
+                            <?php endforeach; ?>
+                        </select>
+                    </div>
+                    <div class="col-md-12">
+                        <label>Item (Current: <?php echo $order['ItemName'] ?>)</label>
+                        <select class="form-select" id="InvItem" name="InvItem" aria-label=".form-select example">
+                            <?php foreach($inv_item as $inv): ?>
+                            <option value="<?php echo $inv['ItemID'] ?>"><?php echo $inv['ItemName'] ?></option>
+                            <?php endforeach; ?>
+                        </select>
+                    </div>
+
+                    <div class="col-md-12">
+                        <label>Order Type (Current: <?php echo $order['OrderType'] ?>)</label>
+                        <select class="form-select" id="OrderType" name="OrderType" aria-label=".form-select example">
+                            <option value="In Process">In Process</option>
+                            <option value="Completed">Completed</option>
+                        </select>
+                    </div>
+                    <div class="col-md-12">
+                        <label>Order Quantity</label>
+                        <input type="text" name="OrderQty" id="OrderQty" class="form-control" value="<?php echo $order['OrderQty'] ?>">
+                    </div>
+
+                    <h3 class="my-3"> Payment Details </h3>
+
+                    <div class="col-md-12">
+                        <label>Payment Status</label>
+                        <input type="text" name="pay_status" id="pay_status" class="form-control" value="<?php echo $order['pay_status'] ?>">
+                    </div>
+
+                    <h3 class="my-3"> Shipping Details </h3>
+                    <div class="col-md-12">
+                        <label>Courier</label>
+                        <input type="text" name="courier_id" id="courier_id" class="form-control" value="<?php echo $order['courier_id'] ?>">
+                    </div>
+                    <div class="col-md-12">
+                        <label>Tracking Number</label>
+                        <input type="text" name="tracking_no" id="tracking_no" class="form-control" value="<?php echo $order['tracking_no'] ?>">
                     </div>
 
                     <div class="button-group float-end">
--- OrdersAdminView.php
+++ OrdersAdminView.php
@@ -53,7 +53,7 @@
                                     <div class="col-12 col-md-6 col-lg-4">
                                         <div class="clean-product-item">
                                             <div class="image"><a href="OrderPageAdmin.php?id=<?php echo $order['OrderID'] ?>"><img class="img-fluid d-block mx-auto rounded" src="<?php echo $order['ItemImg'] ?>" title="<?php echo $order['ItemName'] ?>"></a></div>
-                                            <div class="product-name"><a href="OrderPageAdmin.php?id=<?php echo $order['OrderID'] ?>" style="color: rgb(111,66,193);">Order#<?php echo $order['id'] ?> - <?php echo $order['ItemName'] ?></a></div>
+                                            <div class="product-name"><a href="OrderPageAdmin.php?id=<?php echo $order['OrderID'] ?>" style="color: rgb(111,66,193);">Order#<?php echo $order['OrderID'] ?> - <?php echo $order['ItemName'] ?></a></div>
                                             <hr><h6>Ordered by: <strong> <?php echo $order['c_name'] ?> </strong></h6>
                                             <h6>Quantity: <?php echo $order['OrderQty'] ?></h6>
                                             <h6>Total Cost: 
