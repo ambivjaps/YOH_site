0a571ab77a092ccd8159299106284f1f006f92ea
James Sing <87142332+ambivjaps@users.noreply.github.com>
2023-03-21 15:21:51 +0800

    Updated admin dashboard jumbotrons

--- views/admin_dashboard.php
+++ views/admin_dashboard.php
@@ -59,35 +59,35 @@
 
     <div class="row justify-content-center g-1">
         <div class="col-md-4 my-2">
-            <div class="h-100 p-5 text-white bg-dark rounded-1 yoh-card-ins" style="background-image: linear-gradient(to bottom, rgba(0,0,0,0.4) 0%,rgba(0,0,0,0.5) 100%), url('assets/img/bg_og.jpg');">
+            <div class="h-100 p-5 text-white bg-dark rounded-1 yoh-card-ins" style="background-image: linear-gradient(to bottom, rgba(0,0,0,0.4) 0%,rgba(0,0,0,0.8) 100%), url('assets/img/dashboard/profiles.jpg');">
                 <h1 class="pt-5 my-3 lh-1" style="text-shadow: #000 1px 0 5px;"><i class="fas fa-user-friends"></i> Profiles </h1>
                 <p style="text-shadow: #000 1px 0 5px;"><strong> <?php echo $profiles ?> </strong> records</p>
                 <a class="btn btn-light mt-2" href="CustomerProfileListAdmin.php" role="button">View all <i class="fas fa-caret-right"></i></a>
             </div>
         </div>
         <div class="col-md-4 my-2">
-            <div class="h-100 p-5 text-white bg-dark rounded-1 yoh-card-ins" style="background-image: linear-gradient(to bottom, rgba(0,0,0,0.4) 0%,rgba(0,0,0,0.5) 100%), url('assets/img/bg_og.jpg');">
+            <div class="h-100 p-5 text-white bg-dark rounded-1 yoh-card-ins" style="background-image: linear-gradient(to bottom, rgba(0,0,0,0.4) 0%,rgba(0,0,0,0.8) 100%), url('assets/img/dashboard/inventory.jpg');">
                 <h1 class="pt-5 my-3 lh-1" style="text-shadow: #000 1px 0 5px;"><i class="fas fa-dolly-flatbed"></i> Inventory </h1>
                 <p style="text-shadow: #000 1px 0 5px;"><strong> <?php echo $inventory ?> </strong>records</p>
                 <a class="btn btn-light mt-2" href="Inventory.php" role="button">View all <i class="fas fa-caret-right"></i></a>
             </div>
         </div>
         <div class="col-md-4 my-2">
-            <div class="h-100 p-5 text-white bg-dark rounded-1 yoh-card-ins" style="background-image: linear-gradient(to bottom, rgba(0,0,0,0.4) 0%,rgba(0,0,0,0.5) 100%), url('assets/img/bg_og.jpg');">
+            <div class="h-100 p-5 text-white bg-dark rounded-1 yoh-card-ins" style="background-image: linear-gradient(to bottom, rgba(0,0,0,0.4) 0%,rgba(0,0,0,0.8) 100%), url('assets/img/dashboard/orders.jpg');">
                 <h1 class="pt-5 my-3 lh-1" style="text-shadow: #000 1px 0 5px;"><i class="fas fa-check-square"></i> Orders </h1>
                 <p style="text-shadow: #000 1px 0 5px;"><strong> <?php echo $orders ?> </strong> records</p>
                 <a class="btn btn-light mt-2" href="OrdersAdminView.php" role="button">View all <i class="fas fa-caret-right"></i></a>
             </div>
         </div>
         <div class="col-md-6 my-2">
-            <div class="h-100 p-5 text-white bg-dark rounded-1 yoh-card-ins" style="background-image: linear-gradient(to bottom, rgba(0,0,0,0.4) 0%,rgba(0,0,0,0.5) 100%), url('assets/img/bg_og.jpg');">
+            <div class="h-100 p-5 text-white bg-dark rounded-1 yoh-card-ins" style="background-image: linear-gradient(to bottom, rgba(0,0,0,0.4) 0%,rgba(0,0,0,0.8) 100%), url('assets/img/dashboard/videos.jpg');">
                 <h1 class="pt-5 my-3 lh-1" style="text-shadow: #000 1px 0 5px;"><i class="fas fa-play"></i> Videos </h1>
                 <p style="text-shadow: #000 1px 0 5px;"><strong> <?php echo $videos ?> </strong> records</p>
                 <a class="btn btn-light mt-2" href="VideosAdmin.php" role="button">View all <i class="fas fa-caret-right"></i></a>
             </div>
         </div>
         <div class="col-md-6 my-2">
-            <div class="h-100 p-5 text-white bg-dark rounded-1 yoh-card-ins" style="background-image: linear-gradient(to bottom, rgba(0,0,0,0.4) 0%,rgba(0,0,0,0.5) 100%), url('assets/img/bg_og.jpg');">
+            <div class="h-100 p-5 text-white bg-dark rounded-1 yoh-card-ins" style="background-image: linear-gradient(to bottom, rgba(0,0,0,0.4) 0%,rgba(0,0,0,0.8) 100%), url('assets/img/dashboard/slides.jpg');">
                 <h1 class="pt-5 my-3 lh-1" style="text-shadow: #000 1px 0 5px;"><i class="fas fa-images"></i> Slides </h1>
                 <p style="text-shadow: #000 1px 0 5px;"><strong> <?php echo $slides ?> </strong> records</p>
                 <a class="btn btn-light mt-2" href="SlidesAdmin.php" role="button">View all <i class="fas fa-caret-right"></i></a>
