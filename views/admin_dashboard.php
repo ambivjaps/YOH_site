03f8e60512016a0b57b2b76881a30b3e84c92de6
James Sing <87142332+ambivjaps@users.noreply.github.com>
2023-03-21 14:58:03 +0800

    Updated with latest records

--- views/admin_dashboard.php
+++ views/admin_dashboard.php
@@ -31,6 +31,23 @@
     $result_slide = mysqli_query($con, $slide_count);
     $slides = mysqli_fetch_array($result_slide)[0];
     mysqli_free_result($result_slide);
+
+
+    // latest inventory records
+    $item_inv = "SELECT * FROM inventory_db ORDER BY ItemID DESC LIMIT 3";
+	$result_inv2 = mysqli_query($con, $item_inv);
+	$latest_inv = mysqli_fetch_all($result_inv2, MYSQLI_ASSOC);
+	mysqli_free_result($result_inv2);
+
+    // latest order records
+    $item_order = "SELECT * FROM orders_db INNER JOIN inventory_db 
+    ON orders_db.ItemID = inventory_db.ItemID INNER JOIN cust_profile 
+    ON orders_db.c_id = cust_profile.c_id
+    WHERE cust_profile.cust_status = '1' ORDER BY OrderID DESC LIMIT 3";
+	$result_order2 = mysqli_query($con, $item_order);
+	$latest_order = mysqli_fetch_all($result_order2, MYSQLI_ASSOC);
+	mysqli_free_result($result_order2);
+
 ?>
 
 <div class="container my-5">
@@ -76,9 +93,62 @@
                 <a class="btn btn-light mt-2" href="SlidesAdmin.php" role="button">View all <i class="fas fa-caret-right"></i></a>
             </div>
         </div><hr>
-        <p class="text-muted"> Yarn Over Hook | Website developed by Group#2 of 3-ITA </p>
     </div>
- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-            <script src="assets/js/sessiontimeout.js"></script>
+
+    <div class="row justify-content-center">
+        <div class="col-md-6 my-2">
+            <h3> Latest Inventory Items </h3>
+            <?php foreach($latest_inv as $inv): ?>
+            <div class="card">
+                <div class="row">
+                    <div class="col-md-4 col-4">
+                        <a href="InventoryItem.php?id=<?php echo $inv['ItemID']; ?>">
+                            <div class="card-seg-thumbnail">
+                                <img src="<?php echo $inv['ItemImg']; ?>" class="img-fluid rounded-start" title="<?php echo $inv['ItemName']; ?>" alt="<?php echo $inv['ItemName']; ?>">
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-8 col-8">
+                        <div class="card-body">
+                            <a href="InventoryItem.php?id=<?php echo $inv['ItemID']; ?>"><h6><?php echo $inv['ItemName']; ?></h6></a><hr>
+                            <h6><span class="badge bg-dark"><?php echo $inv['ItemType']; ?></span></h6>
+                            <h6>Price: Php<?php echo $inv['ItemPrice']; ?></h6>
+                            <h6>Quantity: <?php echo $inv['ItemQty']; ?></h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <?php endforeach; ?>
+        </div>
+
+        <div class="col-md-6 my-2">
+            <h3> Latest Orders </h3>
+            <?php foreach($latest_order as $order): ?>
+            <div class="card">
+                <div class="row">
+                    <div class="col-md-4 col-4">
+                        <a href="OrderPageAdmin.php?id=<?php echo $order['OrderID']; ?>">
+                            <div class="card-seg-thumbnail">
+                                <img src="<?php echo $order['ItemImg']; ?>" class="img-fluid rounded-start" title="<?php echo $order['ItemName']; ?>" alt="<?php echo $order['ItemName']; ?>">
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-8 col-8">
+                        <div class="card-body">
+                            <a href="OrderPageAdmin.php?id=<?php echo $order['OrderID']; ?>"><h6><?php echo $order['ItemName']; ?></h6></a><hr>
+                            <h6><span class="badge bg-dark"><?php echo $order['OrderType']; ?></span></h6>
+                            <h6>Ordered by: <?php echo $order['c_name']; ?></h6>
+                            <h6>Total Price: Php<?php echo $order['OrderTotal']; ?></h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <?php endforeach; ?>
+        </div>
+    </div><hr>
+    <p class="text-muted"> Yarn Over Hook | Website developed by Group#2 of 3-ITA </p>  
 </div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="assets/js/sessiontimeout.js"></script>
