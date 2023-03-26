060b7ff063d41ee8dafbb758cd491554960d5837
Ganesh Singh <abellanoj@gmail.com>
2023-03-26 15:49:51 +0800

    FIXED

--- Inventory.php
+++ Inventory.php
@@ -17,36 +17,35 @@ require 'layouts/Header.php';
 
 <title> Inventory | Yarn Over Hook </title>
 
-<!-- <body class="d-flex flex-column min-vh-100"> -->
+<body class="d-flex flex-column min-vh-100">
     
 <?php require 'layouts/nav.php';?>
-<body class="d-flex flex-column min-vh-100">
-    <main class="page catalog-page">
+
+<main class="page catalog-page">
         <section class="clean-block clean-catalog dark" style="background-color:#efe9ef;">
             <div class="container">
                 <div class="block-heading" >
                     <h2 style="margin:40px; color: black;font-size: 50px;font-weight: bold;">Inventory</h2>
                 </div>
-                
                 <div class="content">
                     <div class="row">
                         <div class="col-md-3">
                             <div class="d-none d-md-block">
-                                <form method="POST" action="javascript:void(0);" id="search_form">
+                                <form method="POST" action="javascript:void(0);" id="search_form">   
                                     <div class="filters">
                                         <div class="float-start float-md-end mt-5 mt-md-0 search-area" style="margin-left: 14px;margin-right: -4px;">
-                                        <div class="float-start float-md-end mt-5 mt-md-0 search-area"></div>
-                                    </div>
-
-                                    <div class="filter-item">  
+                                            <div class="float-start float-md-end mt-5 mt-md-0 search-area"></div>
+                                        </div>
+                                        
+                                    <div class="filter-item">
                                         <h3 style="font-size: 32px; font-weight:bold;">Filters</h3>
                                         <div class="d-flex mt-4 mb-5">
-                                        <input class="form-control rounded" type="text" name="searchInput">
+                                            <input class="form-control rounded" type="text" name="searchInput">
                                             <button type="submit" id="searchInventory" class="btn btn-primary" role="button" style="text-align: center;width: 40px;margin-left: 7px;border-color:indigo;background:indigo;">
                                                 <i class="fas fa-search" style="text-align: center;"></i>
                                             </button>
                                             <a class="btn btn-primary" role="button" style="text-align: center;width: 40px;margin-left: 7px;border-color:indigo;background:indigo;" data-bs-target="AddInventoryItem.php" href="AddInventoryItem.php"><i class="fas fa-plus" style="text-align: center;"></i></a>
-                                        </div><hr>
+                                        </div>
                                         <h3 style="font-size: 20px; font-weight:bold;">Categories</h3>
                                         <?php 
                                             foreach ($categories as $key => $category) {
@@ -58,24 +57,28 @@ require 'layouts/Header.php';
                                                     }
                                                 }
                                         ?>
+                                        
                                         <li class="list-group-item">
-                                            <div class="form-check"><label style="font-weight:bold;"><input class="form-check-input" type="checkbox" value="<?php echo $product->cleanString($category['TypeID']); ?>" <?php echo @$categoryCheck; ?> name="category[]" class="sort_rang category"><?php echo ucfirst($category['ItemType']); ?></label></div>
+                                            <div class="checkbox"><label style="font-weight:bold;"><input type="checkbox" value="<?php echo $product->cleanString($category['TypeID']); ?>" <?php echo @$categoryCheck; ?> name="category[]" class="sort_rang category"><?php echo ucfirst($category['ItemType']); ?></label></div>
                                         </li>
-                                        <?php } ?>
-                                    </div>
+
+                                    <?php } ?></div>
+                                        
                                     </div>
                                 </form>
                             </div>
                         </div>
+
                         <div class="col-md-9">
                             <div class="row g-0" id="results"></div>
                         </div>
                     </div>
-                </div>     
+            
+                </div>       
             </div>
         </section>
     </main>
-</body>
+
 <?php require 'layouts/Footer.php';?>
 
 <script src="assets/js/inventory.js"></script>
